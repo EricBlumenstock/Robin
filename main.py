e5fead7f8fdff13dac872a3bd3455c4c0f4e9ef9
Eric Blumenstock <ericblumenstock@gmail.com>
2019-04-25 20:37:28 -0400

    Added some formatting, a comment, and a few settings.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from Robinhood.Robinhood import Robinhood
+from Robinhood import Robinhood
 from alpha_vantage.techindicators import TechIndicators
 from sys import argv
 import settings
 
 rh = Robinhood()
+rh.login(username=settings.rhusername,password=settings.rhpassword)
 ti = TechIndicators(key=settings.alphavantage_key)
 stocksymbols = [str(i) for i in argv[1:]]
 
@@ -53,14 +54,15 @@ class Symbol:
     def call_rsi(self, timeperiod) -> float:
         rsi = [i['RSI'] for i in ti.get_rsi(symbol=self.symbol, interval='1min', time_period=str(timeperiod))[0].values()][0]
         return rsi
-    
+
+    # Where the macd line is compared to the signal line. If positive macd, is above signal line. Higher number indicates more intense trend.
     def computed_macd_signal_diff(self) -> float:
-        return abs((float(self.macd['MACD_Signal'])) - abs(float(self.macd['MACD'])))
+        return abs((float(self.macd['MACD_Signal']))) - abs(float(self.macd['MACD']))
 
     def table(self) -> str:
-        return 'Symbol: {symbol}\r\nLTP: {ltp}\r\nSell: {ask_price} ({ask_size})\r\nBuy: {bid_price} ({bid_size})\r\nMACD: {macddiff}\r\nRSI: {rsi}'\
-            .format(symbol=self.symbol, ltp=self.LTP, ask_price=self.ask_price, ask_size=self.ask_size,
-            bid_price=self.bid_price, bid_size=self.bid_size, macddiff=self.macd_computed, rsi=self.rsi)
+        return 'Symbol: {symbol}\r\nLTP: {ltp:.2f}\r\nSell: {ask_price:.2f} ({ask_size})\r\nBuy: {bid_price:.2f} ({bid_size})\r\nMACD: {macddiff}\r\nRSI: {rsi:.2f}'\
+            .format(symbol=self.symbol, ltp=float(self.LTP), ask_price=float(self.ask_price), ask_size=self.ask_size,
+            bid_price=float(self.bid_price), bid_size=self.bid_size, macddiff=self.macd_computed, rsi=float(self.rsi))
 
 
 if __name__ == '__main__':
